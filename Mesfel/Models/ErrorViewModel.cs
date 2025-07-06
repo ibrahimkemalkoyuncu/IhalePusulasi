941ec89880be929f7660657a5a0b1251040fdd17
ibrahim kema lkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2025-07-06 16:10:29 +0300

    Add project files.

---
+++ Mesfel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mesfel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
